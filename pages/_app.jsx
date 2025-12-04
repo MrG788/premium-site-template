e0cc0f233c6df1524723cbce65f75494ea8ab35e
MrG788 <143547454+MrG788@users.noreply.github.com>
2025-12-04 00:33:12 +0000

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
+import FloatingCta from "../components/FloatingCta";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <FloatingCta />
+    </>
+  );
 }
