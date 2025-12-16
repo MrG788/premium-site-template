ebc9713aa9625fed8643dd551edc2a5dd5b6a1d8
MrG788 <143547454+MrG788@users.noreply.github.com>
2025-12-16 19:13:18 +0000

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,26 @@
 import "../styles/globals.css";
-import FloatingCta from "../components/FloatingCta";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/aos/aos.js";
+    script.onload = () => {
+      if (window.AOS) {
+        window.AOS.init({
+          duration: 800,
+          easing: "ease-out-cubic",
+          once: true,
+        });
+      }
+    };
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
+      <link rel="stylesheet" href="/aos/aos.css" />
       <Component {...pageProps} />
-      <FloatingCta />
     </>
   );
 }
